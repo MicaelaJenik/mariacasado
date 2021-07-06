02f222937f46b2597154fb4b07707a5bf67f46e5
Micaela Jenik <micaela.jenik@gmail.com>
2021-07-06 16:00:56 -0300

    created artists model and controller and built correct routes, adding welcome and press pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'shows/index'
-  get 'shows/show'
-  get 'artworks/index'
-  get 'artworks/show'
+  get 'artists/index'
+  get 'artists/show'
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'press', to: 'pages#press'
+  get 'welcome', to: 'pages#welcome'
+  resources :artists
+  resources :shows
+  # resources :fairs
 end
