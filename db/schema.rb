c432122a84a6bae93b6cb2970b6f931dbd0ec353
Micaela Jenik <micaela.jenik@gmail.com>
2021-07-06 13:39:26 -0300

    adding tailwind

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_07_04_225657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artworks", force: :cascade do |t|
+    t.string "title"
+    t.integer "year"
+    t.integer "height"
+    t.integer "width"
+    t.integer "depth"
+    t.string "technique"
+    t.text "about"
+    t.text "description"
+    t.string "period"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "shows", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.integer "year"
+    t.string "place"
+    t.string "city"
+    t.string "country"
+    t.string "about"
+    t.string "details"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
