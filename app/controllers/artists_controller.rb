3b107a87718e2843e77e23d3ca018769af96f515
Micaela Jenik <micaela.jenik@gmail.com>
2021-07-12 18:29:11 -0300

    forest set up done and tested

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,15 @@
 class ArtistsController < ApplicationController
+  before_action :set_artists
+
   def index
   end
 
   def show
+  end
+
+  private
+
+  def set_artists
+    @artists = Artist.all
   end
 end
