6e89e87f5dfb5939ef3f33f3e7abb4dd6da329ac
Micaela Jenik <micaela.jenik@gmail.com>
2021-07-15 14:43:47 -0300

    fix

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,7 +1,15 @@
 class ArtworksController < ApplicationController
+  before_action :set_artworks
+
   def index
   end
 
   def show
+  end
+
+  private
+
+  def set_artworks
+    @artworks = Artworks.all
   end
 end
