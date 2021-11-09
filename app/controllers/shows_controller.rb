df404bd432ea2afbcd39709a762a468c5155400c
Micaela Jenik <micaela.jenik@gmail.com>
2021-11-09 21:45:33 +0100

    hope its good

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,7 +1,18 @@
 class ShowsController < ApplicationController
+  before_action :set_shows
+
   def index
+    @shows = Show.all
+    # @actual = Show.find()
   end
 
   def show
+    @show = Show.find(params[:id])
   end
+
+  private
+
+  def set_shows
+  end
+
 end
