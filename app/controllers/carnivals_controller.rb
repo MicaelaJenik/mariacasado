d7f0387ade5670b4a30ed202855fd742f7e5b270
Micaela Jenik <micaela.jenik@gmail.com>
2021-11-28 21:55:29 +0100

    maybe did it?

--- app/controllers/carnivals_controller.rb
+++ app/controllers/carnivals_controller.rb
@@ -1,7 +1,21 @@
 class CarnivalsController < ApplicationController
+  before_action :set_carnivals
+  before_action :set_carnival
+
   def index
+    @carnivals = Carnival.all
+    # @actual = Carnival.find()
   end
 
-  def show
+  def carnival
+    @carnival = Carnival.find(params[:id])
+  end
+
+  private
+
+  def set_carnivals
+  end
+
+  def set_carnival
   end
 end
