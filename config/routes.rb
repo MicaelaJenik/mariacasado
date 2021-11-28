9fa2633a9197242c51b18862ff8178b2a06748bf
Micaela Jenik <micaela.jenik@gmail.com>
2021-11-28 12:17:07 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  get 'staffs/index'
-  get 'staffs/show'
   mount ForestLiana::Engine => '/forest'
   root to: 'pages#home'
   get 'press', to: 'pages#press'
-  get 'nosotros', to: 'pages#nosotros'
   resources :artists
   resources :artworks
   resources :shows
   resources :art
+  resources :staff
   # resources :fairs
 end
