c7969fe9072a6a6972e27c01ecdce35df2fb06d4
Micaela Jenik <micaela.jenik@gmail.com>
2021-12-12 19:35:52 +0100

    added photo to artists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_28_203121) do
+ActiveRecord::Schema.define(version: 2021_12_12_183139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2021_11_28_203121) do
     t.string "instagram_url"
     t.string "facebook_url"
     t.string "personal_web_url"
+    t.string "photo"
   end
 
   create_table "artworks", force: :cascade do |t|
