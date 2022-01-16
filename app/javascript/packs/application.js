9005a06cbc3ceddb03e74ba5d5eff4778d1b46bf
Micaela Jenik <micaela.jenik@gmail.com>
2022-01-16 21:32:18 +0100

    popper

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,7 +28,7 @@ import "scripts/index"
 
 // External imports
 import "bootstrap";
-import "../stylesheets/application"
+// import "../stylesheets/application"
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
